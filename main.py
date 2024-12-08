936808ec4fb9f048eb0ff9ff211a85251b863beb
aenoris <aenoriss@gmail.com>
2024-12-07 23:50:31 -0300

    new try

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import os
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 @app.get("/")
-async def root():
-    return {"message": "Hello 12"}
+def root():
+    return {"message": "Hello World"}
