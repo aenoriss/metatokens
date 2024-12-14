5d554fac29f1a3ae892fa3361622b64109c2bff5
aenoris <aenoriss@gmail.com>
2024-12-14 03:21:27 -0300

    xzcaz

--- main.py
+++ main.py
@@ -170,9 +170,11 @@ async def download_and_convert_image(url: str, tokenAddress: str):
         return None
     
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run(
-        app,
-        host="0.0.0.0",
-        port=int(os.getenv("PORT", 8080))
+        "main:app",  
+        host="0.0.0.0",  
+        port=port,
+        reload=False  
     )
 
